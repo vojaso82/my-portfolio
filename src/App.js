11dfb250500d31c5ac5fd7f93aafc5447d72f522
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-21 10:57:02 -0400

    linked

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import AboutMe from './components/AboutMe';
+import MyProjects from './components/MyProjects';
+import ContactMe from './components/ContactMe';
 
 
 function App() {
+
+
+
+
   return (
     <div className="App">
       
+
+      <Home/>
+      <AboutMe/>
+      <MyProjects/>
+      <ContactMe/>
     </div>
   );
 }
