5b346c12321cd146c7f3c96d9aad9d89a5e806f1
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-28 00:03:33 -0400

    aboutpage

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../components/AboutMe.css'
 
 function AboutMe() {
     return (
-        <div className = "about-me">
-            <div >
+     
+        <div className ="main-div-about-me">
+         <div className="aboutme-div">
             <h1>About me</h1>
             <p>I'm a Full Stack Web Developer. </p>
             <p>I worked as a Front End Web Developer back in 2015, when I got 
-            <br/> the chance to move in USA. I am Ironhack boot camp graduated.</p>
+            the chance to move in USA. I am Ironhack boot camp graduated.</p>
             <p>I enjoy spending time coding and designing Full Stack projects.</p>
             <p>Check out my work, my skills aned get in touch with me</p>
+            <div className="tools">
+            <h3>Tools & Technologies</h3>
+            <p>JavaScript(ES6) | React.js | Node.js | Express.js | MongoDB</p>
+            <p>REST API | Mongoose | JSON | CSS | HTML </p>
             </div>
-            
-            <div>
-            <Link to="/projects"> <button>Projects</button></Link> 
-            <Link to="/"> <button>Home</button></Link> 
-            </div>
+         </div>   
+         <div className="buttons-about-div">
+            <Link to="/projects"> Projects</Link> 
+            <Link to="/">Home</Link> 
+        </div>
             
         </div>
+    
     )
 }
 
