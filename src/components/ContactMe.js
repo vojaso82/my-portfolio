74139e4adb4f23dc79197cd613ca3036e34e92c6
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-08-30 10:03:47 -0400

    added contact me section

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -3,6 +3,7 @@ import React from 'react';
 function ContactMe() {
     return (
         <div className="contact-me">
+            <input></input>
             <h1>CONTACT ME</h1>
         </div>
     )
