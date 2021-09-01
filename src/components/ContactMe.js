6e10fd377b585458c72807fc115d0d2cefc34357
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-09-01 18:16:59 -0400

    input form is working

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../components/ContactMe.css'
 import { Spring } from 'react-spring/renderprops';
 import { motion } from 'framer-motion';
 import { useTrail, animated } from 'react-spring';
+import emailjs from 'emailjs-com';
 
 function ContactMe() {
+ const[message, setMessage] = useState();
+ const[name, setName] = useState();
+ const[email, setEmail] = useState();
+ const[phone, setPhone] = useState();
+ const[text, setText] = useState();
+
+
+const handleChange = (e) => {
+if(e.target.name === "name"){
+    setName(e.target.value)
+}else if(e.target.name === "email"){
+    setEmail(e.target.value)
+}else if(e.target.name === "phone"){
+    setPhone(e.target.value)
+}else if(e.target.name === "message"){
+    setText(e.target.value)
+}
+}
+
+
+function sendEmail(e) {
+e.preventDefault();
+if(name && email && phone && text){
+
+emailjs.sendForm('service_ji46uhq', 'template_chppd3c', e.target, 'user_ZyOxWspwe6RKIA4Y8K4xg')
+.then((result) => {
+console.log(result.text);
+result.text === "OK" ? setMessage("Message sent") : setMessage("Please try again")
+}, (error) => {
+console.log(error.text);
+});
+}else setMessage('You have to fill all fields')
+
+
+e.target.reset();
+}
+    
     return (
         <Spring
         from={{opacity:0, marginLeft:-500}}
@@ -16,19 +54,21 @@ function ContactMe() {
         <div className="h1-text">
             <h1>Get in touch with me</h1>
         </div>
-        <form className="inputs-div">
+        <form onSubmit={sendEmail} className="inputs-div">
             
-            <input type="text" name="name" placeholder="You name"/>
-            <input type="text" name="email" placeholder="You email"/>
-            <input type="text" name="phone" placeholder="You phone number"/>
-            <textarea type="text" name="message" placeholder="You message"/>
+            <input onChange={(e)=>handleChange(e)} type="text" name="name" placeholder="You name"/>
+            <input onChange={(e)=>handleChange(e)} type="text" name="email" placeholder="You email"/>
+            <input onChange={(e)=>handleChange(e)} type="text" name="phone" placeholder="You phone number"/>
+            <textarea onChange={(e)=>handleChange(e)} type="text" name="message" placeholder="You message"/>
+            <p>{message}</p>
             <motion.div className='submit-button'
           whileHover={{
             scale:1.05,
             textShadow:"0px 0px 15px rgb(95,244,255)",
             boxShadow:"0px 0px 10px rgb(95,244,255)", 
              }}>
-            <Link >Submit</Link> 
+            
+            <button id="contact-button" type="submit">Send</button>
         </motion.div>
         
         </form> 
