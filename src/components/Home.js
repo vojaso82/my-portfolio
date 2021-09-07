572596b0b8a92091df10f39fa77ae662f146ffb0
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-09-07 19:21:04 -0400

    home page text changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,8 +15,8 @@ function Home() {
       <div style={props} className="main-div">
       <div className="home">
           <h1 className="my-name">Vojislav Zaja</h1>
-            <h2 className="job-title">Web Developer</h2>
-            <p className="description">I define and design customer experiences in the digital channel</p>
+            <h2 className="job-title"> Web Developer based in Miami/FL</h2>
+            <p className="description">I define and design customer experiences in the digital channel.</p>
       </div>
       <div className="buttons-div">
       <motion.div className='motion-button1'
