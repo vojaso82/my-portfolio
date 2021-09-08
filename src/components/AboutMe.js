d09e193936da922f74cd5d9c46260e0ea5779903
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-09-08 09:57:18 -0400

    about me fixed media query

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -17,7 +17,9 @@ function AboutMe() {
         {props => (
         <div style={props} className ="main-div-about-me">
          <div className="aboutme-div">
-            <p>I am Full Stack Web Developer. Graduated from Ironhack boot camp in Miami. </p>
+            <p>I am Full Stack Web Developer.</p>
+            <p>Graduated from Ironhack boot camp in Miami in 2021.</p>
+            <p> Previously I worked as a Front End Web Developer Intern back in 2015. </p>
             <p></p>
             <p>I enjoy spending time coding and designing projects.</p>
             <p>Check out my work, my skills and get in touch with me.</p>
