988aedd12e26948a02472a8e7b91f2deb9c9e783
vojaso82 <vojislavzaja@gmail.com>
2022-12-28 11:34:15 -0500

    Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ function Home() {
       <div style={props} className="main-div">
       <div className="home">
           <h1 className="my-name">Vojislav Zaja</h1>
-            <h2 className="job-title"> Web Developer based in Miami/FL</h2>
+            <h2 className="job-title"> Software Developer based in Miami</h2>
             <p className="description">I love to create and design customer experiences in the digital channel.</p>
       </div>
       <div className="buttons-div">
